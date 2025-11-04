8f94a64e77dd64a8b1b632635d79f03d61ba113f
akhil239 <akhilsonnakula23@email.com>
2025-11-04 08:44:45 +0530

    2

--- main.c
+++ main.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 int main(){
-  printf("hello wrld");
-  printf("tesing");
-  printf("tesing");
-  for(int i = 0; i <= 5;i++){
-    printf("hello");
-  }
-  printf("changed in git");
-  priuntf("anther git");
   return 0;
 };
 
