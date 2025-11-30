6966c6c7ab092330a150b1d91543fce96ff7586c
akhil239 <akhilsonnakula23@email.com>
2025-11-30 18:39:17 +0530

    linearsort

--- main.c
+++ main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-/*function structure in c 
-type_of_the_function name_of_the_function (parameters){
-  body_of_the_function
-} 
-  if it returns int function type must should be int 
-  if it returns none use type of the void means the this function returns nothing
-  if it returns any other type it must match with the return type
-  to store returned value varble must same as return and funtion type 
-*/
-int main(){
-  struct point{
-    int x;
-    int y;
-  };
 
-  struct point Point;
-  Point.x = 5;
-  printf("%d",Point.x);
-  return 0;
-};
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
 
+void linearsort(int arr[] , int n){
+  for(int i = 0; i < n; i++){
+    for (int j = i+1 ; j < n ; j++){
+      if( arr[i]< arr[j]){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+}
+// Driver code to test the insertion sort
 
+int main() {
+    
+    return 0;
+}
