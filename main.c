3ff04efca209e555aa279d86b3231cb2de87a066
white_Mad_666 <85440691+akhil239@users.noreply.github.com>
2025-12-03 06:42:31 +0000

    insertionsort

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<stdlib.h>
 
 // Function to print an array
 void printArray(int arr[], int size) {
@@ -20,10 +21,22 @@ void linearsort(int arr[] , int n){
   }
 }
 // Driver code to test the insertion sort
-void insertion(int arr[],size){
-  ;
+void insertion(int array[],int size){
+  for(int i = 0; i < size;i++){
+    int key  = array[i];
+    int j = i -1;
+    while (j >= 0 && array[j] > key){
+      array[j+1] = array[j];
+      j= j- 1;
+    }
+    array[j+1] = key;
+  }
 }
 int main() {
-    
+  int arr[]= {23654,3525,624,4247,723,546,6435,4367,565,53,5,34,5,525,2324532};
+  int size = sizeof(arr)/sizeof(arr[0]);
+  insertion(arr,size);
+  printArray(arr,size);
+  
     return 0;
 }
